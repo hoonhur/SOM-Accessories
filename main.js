80f4fa9860c18f083868b15ca11f6a72f7b137dd
Hoon Hur <hoon@Hoons-MacBook-Pro.local>
2020-12-15 23:07:14 -0500

    add Earrings

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
   let pathname = url.parse(_url, true).pathname;
-  let description;
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      fs.readdir("./data", (err, filelists) => {
         let title = "welcome";
-        description = "Hello, Node.js";
+        let description = "Hello, Node.js";
+        let list = "<ul>";
+        var i = 0;
+        while (i < filelists.length) {
+          list =
+            list +
+            `<li><a href='/?id=${filelists[i]}'>${filelists[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
         let template = `
-              <!DOCTYPE html>
+            <!DOCTYPE html>
             <html>
               <head>
                 <meta charset="utf-8" />
@@ -25,11 +33,7 @@ let app = http.createServer(function (request, response) {
               <body>
                 <h1><a href="/">SOM Accessories</a></h1>
                 <div id="grid">
-                  <ul>
-                    <li><a href="/?id=Pompoms">Pompoms</a></li>
-                    <li><a href="/?id=Gloves">Gloves</a></li>
-                    <li><a href="/?id=Scarves">Scarves</a></li>
-                  </ul>
+                  ${list}
                   <div id="article">
                     <h2>${title}</h2>
                     <p>${description}</p>
@@ -42,35 +46,45 @@ let app = http.createServer(function (request, response) {
         response.end(template);
       });
     } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        let title = queryData.id;
-        let template = `
-        <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8" />
-          <title>SOM Accessories ${title}</title>
-          <link rel="stylesheet" type="text/css" href="main.css" />
-          <script src="main.js"></script>
-        </head>
-        <body>
-          <h1><a href="/">SOM Accessories</a></h1>
-          <div id="grid">
-            <ul>
-              <li><a href="/?id=Pompoms">Pompoms</a></li>
-              <li><a href="/?id=Gloves">Gloves</a></li>
-              <li><a href="/?id=Scarves">Scarves</a></li>
-            </ul>
-            <div id="article">
-              <h2>${title}</h2>
-              <p>${description}</p>
+      fs.readdir("./data", (err, filelists) => {
+        let list = "<ul>";
+        var i = 0;
+        while (i < filelists.length) {
+          list =
+            list +
+            `<li><a href='/?id=${filelists[i]}'>${filelists[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        fs.readFile(`data/${queryData.id}`, "utf8", function (
+          err,
+          description
+        ) {
+          let title = queryData.id;
+          let template = `
+          <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8" />
+            <title>SOM Accessories ${title}</title>
+            <link rel="stylesheet" type="text/css" href="main.css" />
+            <script src="main.js"></script>
+          </head>
+          <body>
+            <h1><a href="/">SOM Accessories</a></h1>
+            <div id="grid">
+              ${list}
+              <div id="article">
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </div>
             </div>
-          </div>
-        </body>
-      </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+          </body>
+        </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
