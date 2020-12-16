ecd8924dd335168f5ea3f72600d7f92d9c6e28f8
Hoon Hur <hoon@Hoons-MacBook-Pro.local>
2020-12-15 19:58:45 -0500

    modify main.js

--- main.js
+++ main.js
@@ -5,45 +5,77 @@ let url = require("url");
 let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
-  let title = queryData.id;
-  console.log(queryData.id);
+  let pathname = url.parse(_url, true).pathname;
+  let description;
 
-  if (_url == "/") {
-    title = "welcome";
-  }
-  if (_url == "/favicon.ico") {
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        let title = "welcome";
+        description = "Hello, Node.js";
+        let template = `
+              <!DOCTYPE html>
+            <html>
+              <head>
+                <meta charset="utf-8" />
+                <title>SOM Accessories ${title}</title>
+                <link rel="stylesheet" type="text/css" href="main.css" />
+                <script src="main.js"></script>
+              </head>
+              <body>
+                <h1><a href="/">SOM Accessories</a></h1>
+                <div id="grid">
+                  <ul>
+                    <li><a href="/?id=Pompoms">Pompoms</a></li>
+                    <li><a href="/?id=Gloves">Gloves</a></li>
+                    <li><a href="/?id=Scarves">Scarves</a></li>
+                  </ul>
+                  <div id="article">
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                  </div>
+                </div>
+              </body>
+            </html>
+              `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        let title = queryData.id;
+        let template = `
+        <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8" />
+          <title>SOM Accessories ${title}</title>
+          <link rel="stylesheet" type="text/css" href="main.css" />
+          <script src="main.js"></script>
+        </head>
+        <body>
+          <h1><a href="/">SOM Accessories</a></h1>
+          <div id="grid">
+            <ul>
+              <li><a href="/?id=Pompoms">Pompoms</a></li>
+              <li><a href="/?id=Gloves">Gloves</a></li>
+              <li><a href="/?id=Scarves">Scarves</a></li>
+            </ul>
+            <div id="article">
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </div>
+          </div>
+        </body>
+      </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
     response.writeHead(404);
-    response.end();
-    return;
+    response.end("Not found");
   }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, "utf8", function (err, description) {
-    let template = `
-    <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8" />
-      <title>SOM Accessories ${title}</title>
-      <link rel="stylesheet" type="text/css" href="main.css" />
-      <script src="main.js"></script>
-    </head>
-    <body>
-      <h1><a href="/">SOM Accessories</a></h1>
-      <div id="grid">
-        <ul>
-          <li><a href="/?id=Pompoms">Pompoms</a></li>
-          <li><a href="/?id=Gloves">Gloves</a></li>
-          <li><a href="/?id=Scarves">Scarves</a></li>
-        </ul>
-        <div id="article">
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </div>
-      </div>
-    </body>
-  </html>
-    `;
-    response.end(template);
-  });
 });
 app.listen(3000);
